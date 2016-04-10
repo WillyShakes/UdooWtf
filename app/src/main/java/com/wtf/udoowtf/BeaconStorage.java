6ad3809a7b5a8dc73cae4516017ba714cdfb62d7
Giuseppe Nesca <giuseppenesca.g@gmail.com>
2016-04-10 12:44:58 +0200

    adds new MAC ADDRESS

--- app/src/main/java/com/wtf/udoowtf/BeaconStorage.java
+++ app/src/main/java/com/wtf/udoowtf/BeaconStorage.java
@@ -10,7 +10,9 @@ public class BeaconStorage {
 
     public BeaconStorage(){
         beaconList.add(new BeaconDevice("D7:B3:84:F8:C2:4B","A","room1", R.drawable.wtf_ic_phone));
-        beaconList.add(new BeaconDevice("EC:FD:86:8A:92:28","B","room2",R.drawable.wtf_ic_window));
+        beaconList.add(new BeaconDevice("EC:FD:86:8A:92:28","B","room2",R.drawable.wtf_ic_light));
+        beaconList.add(new BeaconDevice("00:07:80:37:8A:6D","IOTSemplice","door",R.drawable.wtf_ic_window));
+        beaconList.add(new BeaconDevice("C9:5D:E2:ED:DC:98","TrackR","portachiavi",R.drawable.wtf_ic_phone));
     }
 
     public ArrayList<BeaconDevice> getBeaconList(){
