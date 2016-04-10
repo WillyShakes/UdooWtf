6fab8e191071f026de9771b60cd213aea7e0fb07
WillyShakes <mbouendaw@yahoo.fr>
2016-04-10 13:41:52 +0200

    New version of the useCase 1 with discovery. @WillyShakes

--- app/src/main/java/com/wtf/udoowtf/Scanner.java
+++ app/src/main/java/com/wtf/udoowtf/Scanner.java
@@ -3,6 +3,12 @@ package com.wtf.udoowtf;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Intent;
 import android.os.Handler;
 import android.util.Log;
@@ -10,6 +16,7 @@ import android.util.Log;
 import junit.framework.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alessandromorelli on 09/04/16.
@@ -27,6 +34,8 @@ public class Scanner {
     private static final long SCAN_PERIOD = 5000;
     private ArrayList<BeaconDevice> beaconList;
     private Activity activity;
+    private BluetoothLeScanner scanner;
+
     public Scanner(Activity context, Handler h, BluetoothAdapter ba) {
         beaconList = new BeaconStorage().getBeaconList();
         mHandler = h;
@@ -43,52 +52,84 @@ public class Scanner {
 
 
     public void BluethootScan() {
-        // Stops scanning after a pre-defined scan period.
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 mScanning = false;
-                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                scanner.stopScan(scanCallback);
             }
         }, SCAN_PERIOD);
         mScanning = true;
-        mBluetoothAdapter.startLeScan(mLeScanCallback);
+        startScanner();
     }
 
-    // Device scan callback.
-    private BluetoothAdapter.LeScanCallback mLeScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
+    private void startScanner() {
+        BluetoothManager bluetoothManager = (BluetoothManager) activity.getSystemService(activity.BLUETOOTH_SERVICE);
+        BluetoothAdapter adapter = bluetoothManager.getAdapter();
+        scanner = adapter.getBluetoothLeScanner();
+        ScanSettings settings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_BALANCED).build();
+       // scanner.startScan(scanFilters(), settings, scanCallback);
+        scanner.startScan(scanCallback);
+        System.out.println("Scanner started");
+    }
 
-                @Override
-                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-                    int toRemove = -1;
-                    for (int i = 0; i < beaconList.size(); i++) {
-                        if (device.getAddress().equals(beaconList.get(i).getMac())) {
-                            toRemove = i;
-                        }
-                    }
+    private List<ScanFilter> scanFilters() {
+        ScanFilter filter;
+        List<ScanFilter> list = new ArrayList<ScanFilter>(new BeaconStorage().getBeaconList().size());
+        for (BeaconDevice d: new BeaconStorage().getBeaconList()) {
+            filter = new ScanFilter.Builder().setDeviceAddress(d.getMac()).build();
+            list.add(filter);
+        }
+        return list;
+    }
 
-                    if (toRemove != -1) {
-                        beaconList.remove(toRemove);
-                    }
 
-                    if(!mScanning){
-                        Log.d(TAG, "scaning stopped");
-                        Intent intent = new Intent(activity, NotificationsActivity.class);
-                        activity.startActivity(intent);
-                        for(int i=0; i<beaconList.size(); i++){
-                            //TODO send an intent for the next activity.
-                            activity.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Log.d(TAG, "startActivity");
-                                    NotificationsActivity.beaconList = beaconList;
-                                    Intent intent = new Intent(activity, NotificationsActivity.class);
-                                    activity.startActivity(intent);
-                                }
-                            });
-                        }
+
+    private final ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Log.d(TAG, "scaning stopped with error" + errorCode);
+            mScanning = false;
+        }
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            Log.d(TAG, "scaning finished: "+device);
+            if (device != null) {
+                int toRemove = -1;
+                for (int i = 0; i < beaconList.size(); i++) {
+                    if (device.getAddress().equals(beaconList.get(i).getMac())) {
+                        toRemove = i;
                     }
                 }
-            };
+
+                if (toRemove != -1) {
+                    beaconList.remove(toRemove);
+
+                }
+
+                if(!mScanning){
+                    Log.d(TAG, "scaning stopped");
+                    for(int i=0; i<beaconList.size(); i++){
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Log.d(TAG, "startActivity");
+                                NotificationsActivity.beaconList = beaconList;
+                                Intent intent = new Intent(activity, NotificationsActivity.class);
+                                activity.startActivity(intent);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    };
+
+
+
+
+
 }
