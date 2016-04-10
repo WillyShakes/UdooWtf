8aa5ef192aa71329f8aa55e8b8b62f2edbb3a0fa
Wilfried Mbouenda Mbogne <WillyShakes@users.noreply.github.com>
2016-04-10 13:00:27 +0200

    Update BeaconStorage.java

--- app/src/main/java/com/wtf/udoowtf/BeaconStorage.java
+++ app/src/main/java/com/wtf/udoowtf/BeaconStorage.java
@@ -9,8 +9,10 @@ public class BeaconStorage {
     private ArrayList<BeaconDevice> beaconList = new ArrayList<BeaconDevice>();
 
     public BeaconStorage(){
-        beaconList.add(new BeaconDevice("D7:B3:84:F8:C2:4B","A","room1"));
-        beaconList.add(new BeaconDevice("EC:FD:86:8A:92:28","B","room2"));
+        beaconList.add(new BeaconDevice("D7:B3:84:F8:C2:4B","A","room1", R.drawable.wtf_ic_phone));
+        beaconList.add(new BeaconDevice("EC:FD:86:8A:92:28","B","room2",R.drawable.wtf_ic_light));
+        beaconList.add(new BeaconDevice("00:07:80:37:8A:6D","IOTSemplice","door",R.drawable.wtf_ic_window));
+        beaconList.add(new BeaconDevice("C9:5D:E2:ED:DC:98","TrackR","portachiavi",R.drawable.wtf_ic_phone));
     }
 
     public ArrayList<BeaconDevice> getBeaconList(){
